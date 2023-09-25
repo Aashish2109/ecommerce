0b5fa99d490e066b11664f3c61e5438a343c7d1a
Aashish Gupta <aashishg863@gmail.com>
2023-09-25 19:47:59 +0530

    updated

--- server.js
+++ server.js
@@ -1,24 +1,23 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY);
+const stripe = require("stripe")(`${process.env.STRIPE_PRIVATE_KEY}`);
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 const path = require("path");
-const { log } = require("console");
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "build", "index.html"));
-  });
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("build"));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "build", "index.html"));
+//   });
+// }
 
 app.get("/", (req, res) => {
-  res.send("Welcome to eShop website.");
-});
+    res.send("Welcome to ecommerce website.")
+})
 
 const array = [];
 const calculateOrderAmount = (items) => {
@@ -30,7 +29,6 @@ const calculateOrderAmount = (items) => {
   const totalAmount = array.reduce((a, b) => {
     return a + b;
   }, 0);
-
   return totalAmount * 100;
 };
 
@@ -40,21 +38,21 @@ app.post("/create-payment-intent", async (req, res) => {
   // Create a PaymentIntent with the order amount and currency
   const paymentIntent = await stripe.paymentIntents.create({
     amount: calculateOrderAmount(items),
-    currency: "usd",
+    currency: "inr",
     automatic_payment_methods: {
       enabled: true,
     },
     description,
     shipping: {
-      address: {
-        line1: shipping.line1,
-        line2: shipping.line2,
-        city: shipping.city,
-        country: shipping.country,
-        postal_code: shipping.postal_code,
-      },
-      name: shipping.name,
-      phone: shipping.phone,
+        address: {
+            line1: shipping.line1,
+            line2: shipping.line2,
+            city: shipping.city,
+            country: shipping.country,
+            postal_code: shipping.postal_code,
+        },
+        name: shipping.name,
+        phone: shipping.phone,
     },
     // receipt_email: customerEmail
   });
@@ -64,5 +62,5 @@ app.post("/create-payment-intent", async (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 4242;
+const PORT = process.env.PORT || 4242
 app.listen(PORT, () => console.log(`Node server listening on port ${PORT}`));
